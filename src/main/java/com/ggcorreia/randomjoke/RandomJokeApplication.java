0f38939d0367a4f0abc94b9ec1e5a1f673a05255
Guilherme Correia <ggcorreia@Karinas-MacBook-Air.local>
2022-06-09 23:20:21 +0200

    Initial commit

---
+++ src/main/java/com/ggcorreia/randomjoke/RandomJokeApplication.java
@@ -0,0 +1,13 @@
+package com.ggcorreia.randomjoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomJokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomJokeApplication.class, args);
+	}
+
+}
